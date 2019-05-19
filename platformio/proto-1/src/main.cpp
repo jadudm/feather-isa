0572cbc954290bdde8e96ea813786df78785a323
Matthew Jadud <matt@jadud.com>
2019-05-18 20:39:24 -0400

    Digital buttons working.
    The analog pins get really noisy when you start driving a Neopixel ring.

--- platformio/proto-1/src/main.cpp
+++ platformio/proto-1/src/main.cpp
@@ -1,14 +1,26 @@
 #include "MIDIUSB.h"
 #include <Adafruit_NeoPixel.h>
 
-#define NEOPIXEL_BOARD_PIN          8
-#define NEOPIXEL_RIBBON_PIN         4
-#define NEOPIXEL_RIBBON_LENGTH    120
-#define MIDI_CHANNEL                0
-#define MIDI_PACKET_SIZE           16
+#define NEOPIXEL_BOARD_PIN 8
+#define NEOPIXEL_RIBBON_PIN 4
+#define NEOPIXEL_RIBBON_LENGTH 120
+#define MIDI_CHANNEL 0
+#define MIDI_PACKET_SIZE 16
 
+// Define if I want to periodically send out all of the
+// analog readings, even if they have not substantively change.
+// #define PERIODIC_ANALOG_REFRESH 1
+
+// https://learn.adafruit.com/adafruit-neopixel-uberguide/arduino-library-use
 Adafruit_NeoPixel board_neo(1, NEOPIXEL_BOARD_PIN, NEO_GRB + NEO_KHZ800);
 Adafruit_NeoPixel ribbon_neo(NEOPIXEL_RIBBON_LENGTH, NEOPIXEL_RIBBON_PIN, NEO_GRB + NEO_KHZ800);
+
+int analog_pins[] = {A0, A1, A2, A3, A4, A5};
+int ANALOG_PIN_COUNT = sizeof(analog_pins) / sizeof(analog_pins[0]);
+int *analog_readings = (int *)malloc(ANALOG_PIN_COUNT * sizeof(int));
+int digital_pins[] = {5, 6, 9, 10, 11, 12, 13};
+int DIGITAL_PIN_COUNT = sizeof(digital_pins) / sizeof(digital_pins[0]);
+int *digital_readings = (int *)malloc(DIGITAL_PIN_COUNT * sizeof(int));
 
 typedef struct
 {
@@ -99,14 +111,17 @@ void read_msgs()
         msg_length = rx.byte3;
         // Serial.println("LENGTH: " + String(msg_length));
         // Serial.println("-> PKTS");
-        if (msg_length > MIDI_PACKET_SIZE) {
+        if (msg_length > MIDI_PACKET_SIZE)
+        {
           state = START;
-        } else {
+        }
+        else
+        {
           state = PKTS;
         }
         break;
       case PKTS:
-        // FIXME: In theory, a timeout could be useful here. 
+        // FIXME: In theory, a timeout could be useful here.
         if (byte_count < msg_length)
         {
           // Serial.println(" R " + String(byte_count) + " " + rx.byte3);
@@ -131,7 +146,7 @@ void read_msgs()
   } while (rx.header != 0);
 }
 
-// / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / 
+// / / / / / / / / / / / / / / / / / / / / / / / / / / / / / /
 // NEOPIXELS
 void board(byte r, byte g, byte b)
 {
@@ -139,7 +154,8 @@ void board(byte r, byte g, byte b)
   board_neo.show();
 }
 
-void ribbon(byte ndx, byte r, byte g, byte b) {
+void ribbon(byte ndx, byte r, byte g, byte b)
+{
   ribbon_neo.setPixelColor(ndx, board_neo.Color(r / 2, g / 2, b / 2));
 }
 
@@ -158,11 +174,12 @@ bool is_valid_packet()
   return (pkt[msg_count].value == crc);
 }
 
-enum MsgTypes {
+enum MsgTypes
+{
   SET_BOARD_PIXEL = 1,
   SET_PIXEL_AT_INDEX = 2,
   SET_PIXEL_RANGE = 3
-  };
+};
 
 void interpret()
 {
@@ -172,26 +189,36 @@ void interpret()
     // The packet is:
     // [0] MSG TYPE
     // [1 ... ] Payload (per message type)
-    switch(pkt[0].value) {
-      case SET_BOARD_PIXEL:
-        board(pkt[1].value, pkt[2].value, pkt[3].value);
+    switch (pkt[0].value)
+    {
+    case SET_BOARD_PIXEL:
+      board(pkt[1].value, pkt[2].value, pkt[3].value);
       break;
-      case SET_PIXEL_AT_INDEX:
-        ribbon(pkt[1].value, pkt[2].value, pkt[3].value, pkt[4].value);
-      break;  
-      case SET_PIXEL_RANGE:
-        // Serial.println("SET_PIXEL_RANGE");
-        int start = pkt[1].value;
-        int end   = pkt[2].value;
-        // Serial.println("S " + String(start) + " E " + String(end));
-        for (int ndx = start ; ndx < end ; ndx++) {
-          ribbon(ndx, pkt[3].value, pkt[4].value, pkt[5].value);
-        } 
-        ribbon_neo.show();  
-        break;
+    case SET_PIXEL_AT_INDEX:
+      ribbon(pkt[1].value, pkt[2].value, pkt[3].value, pkt[4].value);
+      break;
+    case SET_PIXEL_RANGE:
+      // Serial.println("SET_PIXEL_RANGE");
+      int start = pkt[1].value;
+      int end = pkt[2].value;
+      // Serial.println("S " + String(start) + " E " + String(end));
+      for (int ndx = start; ndx < end; ndx++)
+      {
+        ribbon(ndx, pkt[3].value, pkt[4].value, pkt[5].value);
+      }
+      ribbon_neo.show();
+      break;
     }
-
   }
+}
+
+int scale_analog(int reading)
+{
+  // Get a number from zero to one.
+  float tmp = reading / 1024.0;
+  // Scale it to MIDI.
+  tmp = tmp * 127.0;
+  return (int)floor(tmp);
 }
 
 int counter = 0;
@@ -201,31 +228,122 @@ void setup()
   Serial.begin(115200);
   delay(1000);
   Serial.println("ISA MIDI Interface");
+  // Initialize Neopixels
   board_neo.begin();
   ribbon_neo.begin();
+
   // Flush the MIDI buffer, if there is one.
   for (int i = 0; i < 60; i++)
   {
     MidiUSB.read();
   }
+
+  // Setup the analog inputs.
+  // Do an initial read, so the array is not empty.
+  for (int ndx = 0; ndx < ANALOG_PIN_COUNT; ndx++)
+  {
+    pinMode(analog_pins[ndx], INPUT);
+    analog_readings[ndx] = analogRead(analog_pins[ndx]);
+    delay(5);
+  }
+
+  for (int ndx = 0; ndx < DIGITAL_PIN_COUNT; ndx++)
+  {
+    pinMode(digital_pins[ndx], INPUT_PULLUP);
+    digital_readings[ndx] = digitalRead(digital_pins[ndx]);
+  }
 }
 
-void loop()
+void process_midi_in()
 {
   read_msgs();
   if (msg_count > 0)
   {
-    // Serial.println("MSGS " + String(msg_count));
-    for (int ndx = 0; ndx < msg_count; ndx++)
-    {
-      //Serial.println(" NDX " + String(ndx) + " C " + String(pkt[ndx].controller) + " V " + String(pkt[ndx].value));
-    }
     // Assumes that pkt is global, and in good health.
     interpret();
   }
+}
+
+int analog_refresh_count = 0;
+
+void process_analog_inputs()
+{
+  int reading;
+
+  for (int ndx = 0; ndx < ANALOG_PIN_COUNT; ndx++)
+  {
+    reading = analogRead(analog_pins[ndx]);
+    if ((reading > analog_readings[ndx] + 20) || (reading < analog_readings[ndx] - 20))
+    {
+      analog_readings[ndx] = reading;
+      // Serial.println("A" + String(analog_pins[ndx]) + " " + String(reading));
+      controlChange(0, ndx, scale_analog(analog_readings[ndx]));
+    }
+  }
+
+#ifdef PERIODIC_ANALOG_REFRESH
+  analog_refresh_count += 1;
+
+  if ((analog_refresh_count % 10000) == 0)
+  {
+    analog_refresh_count = 0;
+    for (int ndx = 0; ndx < ANALOG_PIN_COUNT; ndx++)
+    {
+      reading = analogRead(analog_pins[ndx]);
+      analog_readings[ndx] = reading;
+      controlChange(0, ndx, scale_analog(analog_readings[ndx]));
+    }
+  }
+#endif
+}
+
+int debounce_clock = 0;
+void initial_digital_read()
+{
+  int reading;
+  if (debounce_clock == 0)
+  {
+    debounce_clock = millis();
+    for (int ndx = 0; ndx < DIGITAL_PIN_COUNT; ndx++)
+    {
+      reading = digitalRead(digital_pins[ndx]);
+      if (reading != digital_readings[ndx]) {
+        digital_readings[ndx] = reading;
+      }
+    }
+  }
+}
+
+void debounce_digital_read()
+{
+  int reading;
+  int now = millis();
+
+  if ((now - debounce_clock) > 50)
+  {
+    debounce_clock = 0;
+    for (int ndx = 0; ndx < DIGITAL_PIN_COUNT; ndx++)
+    {
+      reading = digitalRead(digital_pins[ndx]);
+      // Output if it is still the same.
+      if (reading != digital_readings[ndx])
+      {
+        digital_readings[ndx] = reading;
+        controlChange(0, ndx + DIGITAL_PIN_COUNT, digital_readings[ndx]);
+      }
+    }
+  }
+}
+
+void loop()
+{
+  process_midi_in();
+  process_analog_inputs();
+  initial_digital_read();
+  debounce_digital_read();
 
   counter = counter + 1;
-  if ((counter % 1000000) == 0)
+  if ((counter % 10000) == 0)
   {
     Serial.println(counter);
   }
